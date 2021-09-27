ba3d50a681f688bd248ff7497b0c66d26c5b2824
Gustavo Sampaio <gbritosampaio@gmail.com>
2021-09-27 19:00:36 -0300

    fix codeactions to work with 0.5.1

--- lua/lspsaga/codeaction.lua
+++ lua/lspsaga/codeaction.lua
@@ -60,7 +60,7 @@ local need_check_diagnostic = {
 }
 
 function Action:render_action_virtual_text(line, diagnostics)
-  return function(_, _, actions)
+  return function(_, actions)
     if actions == nil or type(actions) ~= "table" or vim.tbl_isempty(actions) then
       if config.code_action_prompt.virtual_text then
         _update_virtual_text(nil)
@@ -97,7 +97,7 @@ function Action:render_action_virtual_text(line, diagnostics)
 end
 
 function Action:action_callback()
-  return function(_, _, response)
+  return function(_, response)
     if response == nil or vim.tbl_isempty(response) then
       print "No code actions available"
       return
@@ -182,7 +182,7 @@ local action_call_back = function(_, _)
   return Action:action_callback()
 end
 
-local action_vritual_call_back = function(line, diagnostics)
+local action_virtual_call_back = function(line, diagnostics)
   return Action:render_action_virtual_text(line, diagnostics)
 end
 
@@ -295,7 +295,7 @@ lspaction.code_action_prompt = function()
   local winid = get_current_winid()
   Action[winid] = Action[winid] or {}
   Action[winid].lightbulb_line = Action[winid].lightbulb_line or 0
-  Action:code_action(action_vritual_call_back, diagnostics)
+  Action:code_action(action_virtual_call_back, diagnostics)
 end
 
 lspaction.do_code_action = function()

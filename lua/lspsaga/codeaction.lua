4a5d498e465f07bc72613db3418ded58c8da6d92
Tami <65782666+tami5@users.noreply.github.com>
2021-10-16 07:18:27 +0300

    ref(codeaction): move some code to codeaction.lua

--- lua/lspsaga/codeaction.lua
+++ lua/lspsaga/codeaction.lua
@@ -1,7 +1,37 @@
+local wrap = require "lspsaga.wrap"
 local libs = require "lspsaga.libs"
 local window = require "lspsaga.codeaction.window"
 local api = require "lspsaga.api"
 local M = {}
+
+local on_code_action_response = function(ctx)
+  window.bufnr = vim.fn.bufnr()
+  window.ctx = ctx
+  window.content, window.actions = { window.title }, {}
+
+  return function(response)
+    for client_id, result in pairs(response or {}) do
+      for index, action in ipairs(result.result or {}) do
+        table.insert(window.actions, { client_id, action })
+        table.insert(window.content, "[" .. index .. "]" .. " " .. action.title)
+      end
+    end
+
+    if #window.actions == 0 or #window.content == 1 then
+      vim.notify("No code actions available", vim.log.levels.INFO)
+      return
+    end
+
+    table.insert(window.content, 2, wrap.add_truncate_line(window.content))
+
+    window.open {
+      contents = window.content,
+      filetype = "LspSagaCodeAction",
+      enter = true,
+      highlight = "LspSagaCodeActionBorder",
+    }
+  end
+end
 
 M.range_code_action = function(context, start_pos, end_pos)
   local active, msg = libs.check_lsp_active()
@@ -12,9 +42,7 @@ M.range_code_action = function(context, start_pos, end_pos)
   api.code_action_request {
     params = vim.lsp.util.make_given_range_params(start_pos, end_pos),
     context = context,
-    callback = function(ctx)
-      return window.prepare(ctx)
-    end,
+    callback = on_code_action_response,
   }
 end
 
@@ -25,9 +53,7 @@ M.code_action = function()
   end
   api.code_action_request {
     params = vim.lsp.util.make_range_params(),
-    callback = function(ctx)
-      return window.prepare(ctx)
-    end,
+    callback = on_code_action_response,
   }
 end
 
--- lua/lspsaga/codeaction/window.lua
+++ lua/lspsaga/codeaction/window.lua
@@ -1,22 +1,18 @@
 local config = require("lspsaga").config_values
-local wrap = require "lspsaga.wrap"
 local window = require "lspsaga.window"
 local libs = require "lspsaga.libs"
-local apply_keys = libs.apply_keys "codeaction.window"
+
 local api = require "lspsaga.api"
+local M = { title = config.code_action_icon .. "CodeActions:" }
 
-local M = {
-  title = config.code_action_icon .. "CodeActions:",
-  actions = {},
-  bufnr = 0,
-  action_bufnr = 0,
-  action_winid = 0,
-  content = {},
-}
+M.apply_keys = function()
+  libs.apply_keys("codeaction.window", {
+    ["close"] = config.code_action_keys.quit,
+    ["execute"] = config.code_action_keys.exec,
+  })
+end
 
-M.__index = M
-
-M.create_window = function(opts)
+M.open = function(opts)
   local cmd, hl = vim.api.nvim_command, vim.api.nvim_buf_add_highlight
   M.action_bufnr, M.action_winid = window.create_win_with_border(opts)
 
@@ -28,56 +24,7 @@ M.create_window = function(opts)
   for i = 1, #M.content - 2, 1 do
     hl(M.action_bufnr, -1, "LspSagaCodeActionContent", 1 + i, 0, -1)
   end
-end
-
-M.setup_actions = function(response)
-  for client_id, result in pairs(response or {}) do
-    for index, action in ipairs(result.result or {}) do
-      table.insert(M.actions, { client_id, action })
-      table.insert(M.content, "[" .. index .. "]" .. " " .. action.title)
-    end
-  end
-
-  if #M.actions == 0 then
-    vim.notify("No code actions available", vim.log.levels.INFO)
-    return
-  end
-end
-
-M.attach_mappings = function()
-  for func, keys in pairs {
-    ["close"] = config.code_action_keys.quit,
-    ["execute"] = config.code_action_keys.exec,
-  } do
-    apply_keys(func, keys)
-  end
-end
-
-M.prepare = function(ctx)
-  M.bufnr = vim.fn.bufnr()
-  M.current_ctx = ctx
-  return M.open
-end
-
-M.open = function(response)
-  M.content = {
-    M.title,
-  }
-  M.setup_actions(response)
-  if #M.content == 1 then
-    return
-  end
-
-  table.insert(M.content, 2, wrap.add_truncate_line(M.content))
-
-  M.create_window {
-    contents = M.content,
-    filetype = "LspSagaCodeAction",
-    enter = true,
-    highlight = "LspSagaCodeActionBorder",
-  }
-
-  M.attach_mappings()
+  M.apply_keys()
 end
 
 M.set_cursor = function()
@@ -96,16 +43,16 @@ end
 M.close = function()
   if M.action_bufnr ~= 0 and M.action_winid ~= 0 then
     window.nvim_close_valid_window(M.action_winid)
-    M.actions = {}
-    M.bufnr, M.action_bufnr, M.action_winid, M.current_ctx = 0, 0, 0, nil
+    M.actions, M.bufnr, M.action_bufnr, M.action_winid, M.ctx = {}, 0, 0, 0, nil
   end
 end
 
 M.execute = function()
-  local number = tonumber(vim.fn.expand "<cword>")
-  local choice = M.actions[number]
-  local client_id, action = choice[1], choice[2]
-  api.code_action_execute(client_id, action, M.current_ctx)
+  local choice = M.actions[tonumber(vim.fn.expand "<cword>")]
+  if choice then
+    local client_id, action = choice[1], choice[2]
+    api.code_action_execute(client_id, action, M.ctx)
+  end
   M.close()
 end
 

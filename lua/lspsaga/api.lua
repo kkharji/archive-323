66656173d7798d32aadc7786d7b390cbf903c2cd
Koichi Shiraishi <zchee.io@gmail.com>
2022-05-04 02:18:09 +0900

    Use Provider to capabilities again
    Co-authored-by: Pedro Sanchez <pedro13jzc@gmail.com>

--- lua/lspsaga/api.lua
+++ lua/lspsaga/api.lua
@@ -34,7 +34,7 @@ M.code_action_execute = function(client_id, action, ctx)
   if
     not action.edit
     and client
-    and type(client.server_capabilities.code_action) == "table"
+    and type(client.server_capabilities.codeActionProvider) == "table"
     and client.server_capabilities.codeActionProvider.resolveProvider
   then
     client.request("codeAction/resolve", action, function(err, resolved_action)
